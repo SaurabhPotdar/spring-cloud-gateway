e81cfa2f20cfba536caadbfcefb6a92754c7d21d
Saurabh Potdar <93652523+tce-saurabhp@users.noreply.github.com>
2022-04-26 18:38:31 +0530

    Initial commit

---
+++ microservice-1/src/main/java/com/tce/controller/SimpleController.java
@@ -0,0 +1,26 @@
+package com.tce.controller;
+
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.server.reactive.ServerHttpRequest;
+import org.springframework.http.server.reactive.ServerHttpResponse;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import reactor.core.publisher.Mono;
+
+@RestController
+@RequestMapping("/ms1")
+public class SimpleController {
+
+    @GetMapping(value = "/get")
+    public Mono<String> getData(ServerHttpRequest request, ServerHttpResponse response) {
+        System.out.println("Inside MS1 getData method");
+        HttpHeaders headers = request.getHeaders();
+
+        headers.forEach((k,v)->{
+            System.out.println(k + " : " + v);
+        });
+
+        return Mono.just("Hello from Reactive SC-MS1 getData method!!");
+    }
+}
